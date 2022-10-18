b94bea097a075b9ee7f5248539ddb898a28a8f40
dluppoli <davide@davideluppoli.it>
2022-10-18 21:36:50 +0200

    Add project files.

---
+++ Exceptions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte num1 = 30;
+            byte num2 = 60;
+
+            int risultato = Utils.CalcolaRisultato(num1, num2);
+
+            Console.WriteLine($"Il risultato è: {risultato}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exceptions/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    public static class Utils
+    {
+        internal static byte CalcolaRisultato(byte num1, byte num2)
+        {
+            return Convert.ToByte(num1 * num2);
+        }
+    }
+}
